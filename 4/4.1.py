3e41be8244f375ca8f32068e4d576a2c67031ebf
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-10 11:50:35 +0300

    add 4.1 solution

---
+++ 4/4.1.py
@@ -0,0 +1,60 @@
+from operator import itemgetter
+from typing import List
+
+
+def get_input_data():
+    n = int(input())
+    segments = []
+    for i in range(n):
+        a, b = map(int, input().split())
+        segments.append([a, b])
+    return n, segments
+
+
+def segment_contains_point(point: int, segment: List[int]) -> bool:
+    a, b = segment
+    return a <= point <= b
+
+
+def main():
+    n, segments = get_input_data()
+
+    segments = sorted(segments, key=itemgetter(1))
+    res = []
+    s = 0
+    while s < len(segments):
+        point = segments[s][1]
+        res.append(point)
+        while s < len(segments) and segment_contains_point(point, segments[s]):
+            s += 1
+
+    print(len(res))
+    print(*res)
+
+
+if __name__ == "__main__":
+    main()
+
+    import sys
+    from io import StringIO
+
+    print("test 1")
+    input_data = """\
+3
+1 3
+2 5
+3 6
+"""
+    sys.stdin = StringIO(input_data)
+    main()
+
+    print("test 2")
+    input_data = """\
+4
+4 7
+1 3
+2 5
+5 6
+    """
+    sys.stdin = StringIO(input_data)
+    main()
