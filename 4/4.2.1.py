4014ebb7decb3ace6c2ea2bc089aed49e7fa43a5
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-11 15:26:01 +0300

    Add 4.2.1 solution

--- 4/4.2.1.py
+++ 4/4.2.1.py
@@ -1,9 +1,36 @@
-from collections import Counter
+import heapq
+from collections import Counter, namedtuple
 from typing import Dict
 
 
+class Node(namedtuple("Node", ["left", "right"])):
+    def traverse(self, code: Dict, acc: str):
+        self.left.traverse(code, acc + "0")
+        self.right.traverse(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def traverse(self, code: Dict, acc: str):
+        code[self.char] = acc or "0"
+
+
 def make_huffman_codes(s: str) -> Dict:
-    return {c: c for c in Counter(s).keys()}
+    h = []
+    for char, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(char)))
+    heapq.heapify(h)
+    counter = len(h)
+    while len(h) > 1:
+        freq1, _c1, left = heapq.heappop(h)
+        freq2, _c2, right = heapq.heappop(h)
+        new_tuple = freq1 + freq2, counter, Node(left, right)
+        heapq.heappush(h, new_tuple)
+        counter += 1
+
+    freq, _c, root = heapq.heappop(h)
+    codes = {}
+    root.traverse(codes, "")
+    return codes
 
 
 def main():
@@ -13,7 +40,16 @@ def main():
     print(len(codes), len(encoded_string))
     for char in sorted(codes):
         print(f"{char}: {codes[char]}")
+    print(encoded_string)
 
 
 if __name__ == "__main__":
+    main()
+
+    import sys
+    from io import StringIO
+
+    print("test 1")
+    input_data = "abacabad"
+    sys.stdin = StringIO(input_data)
     main()
