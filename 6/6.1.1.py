c4138435bf57a81e534a6e8f418609dc3a98d85c
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-11 18:32:51 +0300

    add TLE solution

--- 6/6.1.1.py
+++ 6/6.1.1.py
@@ -1,22 +1,38 @@
 import sys
+from typing import List
 
 
 def input_data():
-    data = [line for line in sys.stdin]
-    array = [int(x) for x in data[0].split()]
-    ks = [int(x) for x in data[1].split()]
-    ks = [len(ks)] + ks
+    data = (line for line in sys.stdin)
+    array = [int(x) for x in next(data).split()]
+    ks = [int(x) for x in next(data).split()]
     return array, ks
+
+
+def binary_search(k: int, array: List[int]) -> int:
+    l = 1
+    r = len(array) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if array[m] == k:
+            return m
+        if k < array[m]:
+            r = m - 1
+        elif k > array[m]:
+            l = l + 1
+    return -1
 
 
 def main():
     array, ks = input_data()
-    print(array)
-    print(ks)
+    res = [binary_search(ks[i], array) for i in range(1, len(ks))]
+    print(*res)
 
 
 if __name__ == "__main__":
-    # main()
+    main()
+
+    assert binary_search(8, [5, 1, 5, 8, 12, 13]) == 3
 
     from io import StringIO
 
